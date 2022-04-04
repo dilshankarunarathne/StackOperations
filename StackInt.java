cea69866b221ccb397c2c603124c1351f4d0d28f
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-04-04 14:53:22 +0530

    Init

---
+++ StackInt.java
@@ -0,0 +1,30 @@
+class StackInt
+{
+	int top = -1;
+	int limit = 1000;
+	int myStack[] = new int[limit];
+	
+	//Insert elements
+	public void push()
+	{
+		// Code here....
+	}
+	
+	//Remeove elements
+	public int pop()
+	{
+		// Code here....
+	}
+	
+	//Check whether the stack is full
+	public boolean isFull()
+	{
+		// Code here....
+	}
+	
+	//Check whether the stack is empty
+	public boolean isEmpty()
+	{
+		// Code here....
+	}
+}
---
+++ StackIntMain.java
@@ -0,0 +1,10 @@
+class StackIntMain1
+{
+	public static void main(String args[])
+	{
+		StackInt s1 = new StackInt();
+		
+		// Call the methods here....
+		
+	}
+}
