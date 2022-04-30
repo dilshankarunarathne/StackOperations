430200ed5603747f27f105075ee5c8cd0d7d5348
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-04-30 15:38:35 +0530

    Update StackIn.java

--- me/karunarathne/CO1212/StackOperations/StackIn.java
+++ me/karunarathne/CO1212/StackOperations/StackIn.java
@@ -10,9 +10,7 @@ public interface StackIn {
     public int pop() ;
 
     // Check whether the stack is full
-    public boolean isFull() {
-        return pointer == 1000 ;
-    }
+    public boolean isFull() ;
 
     // Check whether the stack is empty
     public boolean isEmpty() {
