f7d99ad9c5d481874e4f689310c9f58a07492c4f
Dilshan Karunarathne <maleeshasparrow@gmail.com>
2022-04-30 15:38:11 +0530

    Update StackIn.java

--- me/karunarathne/CO1212/StackOperations/StackIn.java
+++ me/karunarathne/CO1212/StackOperations/StackIn.java
@@ -1,5 +1,27 @@
 package me.karunarathne.CO1212.StackOperations;
 
 public interface StackIn {
+    public int peek () ;
 
+    // Insert elements
+    public void push(int element) {
+        myStack [pointer] = element ;
+        pointer ++ ;
+    }
+
+    // Remove elements
+    public int pop() {
+        pointer -- ;
+        return myStack [pointer] ;
+    }
+
+    // Check whether the stack is full
+    public boolean isFull() {
+        return pointer == 1000 ;
+    }
+
+    // Check whether the stack is empty
+    public boolean isEmpty() {
+        return pointer == 0 ;
+    }
 }
